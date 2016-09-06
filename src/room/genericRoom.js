023982203a21f2ad80a1fb8b2fc596bb14988d40
Frederikam <frogkr@gmail.com>
2016-09-06 12:05:34 +0200

    Added the GenericRoom object

---
+++ src/room/genericRoom.js
@@ -0,0 +1,14 @@
+module.exports = function(x, y, desc){
+	this.x = x;
+	this.y = y;
+	this.desc = desc;
+
+	this.canMove =  function(direction){
+
+	};
+
+	this.getDescription = function(){
+
+	};
+
+}
